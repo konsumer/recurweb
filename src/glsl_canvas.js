08979d292c1a80275d4592cf0f799c741f0577d5
David Konsumer <konsumer@jetboystudio.com>
2024-09-13 15:42:11 -0700

    provide uniforms for expansion

--- src/glsl_canvas.js
+++ src/glsl_canvas.js
@@ -33,15 +33,34 @@ export default class GlslCanvas {
     this.scene = new THREE.Scene()
     this.clock = new THREE.Clock()
 
+    // here my uniforms are hard-coded
     const uniforms = {
       u_time: { type: 'f', value: 0.0 },
       u_resolution: { type: 'v2', value: new THREE.Vector2(canvas.width, canvas.height) },
+
+      // only first 2 are used in recur shaders, but this might be useful if extended
       u_tex0: { type: 'sampler2D' },
       u_tex1: { type: 'sampler2D' },
+      u_tex2: { type: 'sampler2D' },
+      u_tex3: { type: 'sampler2D' },
+      u_tex4: { type: 'sampler2D' },
+      u_tex5: { type: 'sampler2D' },
+      u_tex6: { type: 'sampler2D' },
+      u_tex7: { type: 'sampler2D' },
+      u_tex8: { type: 'sampler2D' },
+      u_tex9: { type: 'sampler2D' },
+
+      // only first 4 are used in recur shaders, but this might be useful if extended
       u_x0: { type: 'f', value: 0.0 },
       u_x1: { type: 'f', value: 0.0 },
       u_x2: { type: 'f', value: 0.0 },
-      u_x3: { type: 'f', value: 0.0 }
+      u_x3: { type: 'f', value: 0.0 },
+      u_x4: { type: 'f', value: 0.0 },
+      u_x5: { type: 'f', value: 0.0 },
+      u_x6: { type: 'f', value: 0.0 },
+      u_x7: { type: 'f', value: 0.0 },
+      u_x8: { type: 'f', value: 0.0 },
+      u_x9: { type: 'f', value: 0.0 }
     }
 
     this.material = new THREE.ShaderMaterial({
@@ -58,17 +77,20 @@ export default class GlslCanvas {
     this.render()
   }
 
+  // called every frame
   render () {
     this.material.uniforms.u_time.value += this.clock.getDelta()
     this.renderer.render(this.scene, this.camera)
     requestAnimationFrame(this.render)
   }
 
+  // load new vertex shader
   load (shader) {
     this.material.fragmentShader = shader
     this.material.needsUpdate = true
   }
 
+  // manually load texture into material (like for file-upload)
   loadTexture (name, url, type) {
     if (type.startsWith('image')) {
       this.material.uniforms[name].value = new THREE.TextureLoader().load(url)
@@ -82,10 +104,8 @@ export default class GlslCanvas {
     }
   }
 
+  // set a named uniform
   setUniform (name, value) {
-    if (name.startsWith('u_tex')) {
-      this.loadTexture(name, value)
-    }
     this.material.uniforms[name].value = value
     this.material.needsUpdate = true
   }
--- src/prism/live/util.js
+++ src/prism/live/util.js
@@ -1,49 +1,31 @@
-import $ from "https://v2.blissfuljs.com/src/$.js";
-import $$ from "https://v2.blissfuljs.com/src/$$.js";
-import create from "https://v2.blissfuljs.com/src/dom/create.js";
-import bind from "https://v2.blissfuljs.com/src/events/bind.js";
-import load from "https://v2.blissfuljs.com/src/async/load.js";
+import $ from 'https://v2.blissfuljs.com/src/$.js'
+import $$ from 'https://v2.blissfuljs.com/src/$$.js'
+import create from 'https://v2.blissfuljs.com/src/dom/create.js'
+import bind from 'https://v2.blissfuljs.com/src/events/bind.js'
+import load from 'https://v2.blissfuljs.com/src/async/load.js'
 
-Object.assign($, {create, bind, load});
-export { $, $$, create, bind, load};
+Object.assign($, { create, bind, load })
+export { $, $$, create, bind, load }
 
 /**
  * Utility for regexp construction
  * @param {*} s
  * @returns
  */
-let escape = s => s.replace(/[-\/\\^$*+?.()|[\]{}]/g, "\\$&");
-let _regexp = (flags, strings, ...values) => {
-	let pattern = strings[0] + values.map((v, i) => escape(v) + strings[i + 1]).join("");
-	return RegExp(pattern, flags);
-};
-let cache = {};
+const escape = s => s.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&')
+const _regexp = (flags, strings, ...values) => {
+  const pattern = strings[0] + values.map((v, i) => escape(v) + strings[i + 1]).join('')
+  return RegExp(pattern, flags)
+}
+const cache = {}
 
-export const regexp = new Proxy(_regexp.bind(this, ""), {
-	get: (t, property) => {
-		return t[property] || cache[property]
-				|| (cache[property] = _regexp.bind(this, property));
-	}
-});
+export const regexp = new Proxy(_regexp.bind(this, ''), {
+  get: (t, property) => {
+    return t[property] || cache[property] ||
+				(cache[property] = _regexp.bind(this, property))
+  }
+})
 
 export function loadLanguages (ids, PrismLive) {
-	ids = Array.isArray(ids) ? ids : ids.split(/,/);
-	return ids.map(c => import(`./languages/${c}.mjs`).then(m => {
-		if (m.default) {
-			PrismLive.registerLanguage(m.default.id, m.default);
-		}
-		else {
-			// Many languages
-			for (let id in m) {
-				if (PrismLive.languages[id]) {
-					// Already registered, augment it
-					Object.assign(PrismLive.languages[id], m[id]);
-				}
-				else {
-					PrismLive.registerLanguage(id, m[id]);
-				}
-
-			}
-		}
-	}));
+  // NOOP: I will load my own languages
 }
